032263635f35416eb390311bd01fe2f2f53a5ba8
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2020-03-16 20:07:12 +0700

    update

--- New folder/SSD1306SimpleDemo/SSD1306SimpleDemo.ino
+++ New folder/SSD1306SimpleDemo/SSD1306SimpleDemo.ino
@@ -8,33 +8,53 @@ SSD1306Wire display(0x3c, D7, D6);
 #error Select ESP8266 board.
 #endif
 int relay = 5;
+int button = 0;
 DHTesp dht;
 float humidity, temperature;
 String role;
+int status1, t = 0;
 void setup() {
   Serial.begin(115200);
   dht.setup(14, DHTesp::DHT11);
+  pinMode(button, INPUT);
+  pinMode(relay, OUTPUT);
   display.init();
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
-  pinMode(relay, OUTPUT);
+
 }
 void loop() {
-  delay(dht.getMinimumSamplingPeriod());
+  //delay(dht.getMinimumSamplingPeriod());
   humidity = dht.getHumidity();
   temperature = dht.getTemperature();
-  if (temperature >= 30) {
-    if (digitalRead(relay) == LOW) {
-      digitalWrite(relay, HIGH);
-      role = "ON";
+  oled();
+  int buttonState = digitalRead(button);
+  status1 = chong_doi();
+  if (status1 == true)
+  {
+    {
+      t = !t; // đảo trạng thái
     }
-  } else {
+    while (status1 == true) {
+      status1 = chong_doi();
+    }
+  }
+  if (t == 1)
+  {
     digitalWrite(relay, LOW);
     role = "OFF";
   }
-  ole();
+  else
+  {
+    if (temperature >= 30) {
+      digitalWrite(relay, LOW);
+    } else {
+      digitalWrite(relay, HIGH);
+    }
+    role = "ON";
+  }
 }
-void ole() {
+void oled() {
   delay(1000);
   display.clear();
   display.drawLine(0, 12, 128, 12);
@@ -49,6 +69,11 @@ void ole() {
   display.drawString(5, 12, String(temperature, 0) + " c");
   display.drawString(75, 12, String(humidity, 0));
   display.setFont(ArialMT_Plain_16);
-  display.drawString(0, 37, "FAN CASE: "+role);
+  display.drawString(0, 37, "AUTO FAN:" + role);
   display.display();
+}
+boolean chong_doi()
+{
+  int sta = !digitalRead(button);
+  return sta;
 }
