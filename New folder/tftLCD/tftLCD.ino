2d46da23910d3163da58bcee177b7bbc553d5c55
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2020-04-03 00:08:48 -0700

    k

--- New folder/tftLCD/tftLCD.ino
+++ New folder/tftLCD/tftLCD.ino
@@ -38,79 +38,75 @@ void setup() {
 }
 
 void loop() {
-  //heder
-  tft.fillScreen(BLACK);
-  tft.setTextColor(WHITE);
-  showmsgXY(135, 16, 1, &FreeSerif12pt7b, "MCU Hardware Monitor");
-  tft.drawFastHLine(0, 17, tft.width(), WHITE);
-  //CPU
-  tft.setTextColor(CYAN);
-  showmsgXY(10, 38, 1, &FreeSans12pt7b, "Intel Core i5-9400F");
-  showmsgXY(1, 70, 1, &FreeSans12pt7b, "Load :");
-  showmsgXY(198, 74, 1, &FreeSans12pt7b, "%");
-  showmsgXY(1, 105, 1, &FreeSans12pt7b, "Temp:");
-  showmsgXY(198, 113, 1, &FreeSans12pt7b, "*C");
-  tft.setTextColor(YELLOW);
-  tft.setCursor(80, 75);
-  tft.setTextSize(2);
-  tft.print("100");
-  tft.setCursor(80, 113);
-  tft.setTextSize(2);
-  tft.print("100");
-  tft.drawFastHLine(0, 40, 230 , CYAN);
-  tft.drawRoundRect(0, 19, 230, 100, 5, CYAN);
-  //GPU
-  tft.setTextColor(RED);
-  showmsgXY(238, 38, 1, &FreeSans12pt7b, "Radeon RX580 Series");
-  showmsgXY(232, 70, 1, &FreeSans12pt7b, "Load :");
-  showmsgXY(445, 74, 1, &FreeSans12pt7b, "%");
-  showmsgXY(232, 105, 1, &FreeSans12pt7b, "Temp:");
-  showmsgXY(445, 113, 1, &FreeSans12pt7b, "*C");
-  tft.setTextColor(YELLOW);
-  tft.setCursor(320, 75);
-  tft.setTextSize(2);
-  tft.print("100");
-  tft.setCursor(320, 113);
-  tft.setTextSize(2);
-  tft.print("100");
-  tft.drawFastHLine(231, 40, 249 , RED);
-  tft.drawRoundRect(231, 19, 249, 200, 5, RED);
-  //Ram
-  tft.setTextColor(GREEN);
-  showmsgXY(90, 140, 1, &FreeSans12pt7b, "RAM");
-  tft.setTextColor(YELLOW);
-  tft.setCursor(8, 165);
-  tft.setTextSize(1);
-  tft.print("USED: 00.0/00.0 GB");
-  tft.drawFastHLine(0, 143, 230 , GREEN);
-  tft.drawRoundRect(0, 120, 230, 50, 5, GREEN);
-  //internet
-  tft.setTextColor(PURPLE);
-  showmsgXY(60, 190, 1, &FreeSans12pt7b, "INTERNET");
-  tft.setTextColor(YELLOW);
-  tft.setCursor(40, 213);
-  tft.setTextSize(1);
-  tft.print("0.00/0.00 Mbps");
-  tft.drawFastHLine(0, 193, 230 , PURPLE);
-  tft.drawRoundRect(0, 170, 230, 50, 5, PURPLE);
-  //Time
-  tft.setTextColor(WHITE);
-  showmsgXY(90, 239, 1, &FreeSans12pt7b, "TIME");
-  showmsgXY(-1, 295, 1, &FreeSevenSegNumFont, "12 59 59");
-  showmsgXY(45, 316, 1, &FreeSans12pt7b, "12/12/2020");
-  tft.drawFastHLine(0, 241, 230 , WHITE);
-  tft.drawRoundRect(0, 220, 230, 100, 5, WHITE);
-  //Temp
-  showmsgXY(232, 262, 1, &FreeSans12pt7b, "IN ROOM: ");
-  showmsgXY(232, 292, 1, &FreeSans12pt7b, "IN CASE: ");
-  tft.drawFastHLine(231, 241, 249 , RED);
-  tft.drawRoundRect(231, 220, 249, 100, 5, RED);
+  boder();
   delay(10000);
 }
-void showmsgXY(int x, int y, int sz, const GFXfont *f, const char *msg)
+void boder() {
+  //heder
+  tft.fillScreen(BLACK);
+  showmsgXY(135, 16, 1, &FreeSerif12pt7b, "MCU Hardware Monitor", WHITE, 0, 0);
+  tft.drawFastHLine(0, 17, tft.width(), WHITE);
+  //CPU
+  showmsgXY(10, 38, 1, &FreeSans12pt7b, "Intel Core i5-9400F", CYAN, 0, 0);
+  showmsgXY(1, 70, 1, &FreeSans12pt7b, "Load :", CYAN, 0, 0);
+  showmsgXY(198, 74, 1, &FreeSans12pt7b, "%", CYAN, 0, 0);
+  showmsgXY(1, 105, 1, &FreeSans12pt7b, "Temp:", CYAN, 0, 0);
+  showmsgXY(198, 113, 1, &FreeSans12pt7b, "*C", CYAN, 0, 0);
+  tft.drawFastHLine(0, 40, 230 , CYAN);
+  tft.drawRoundRect(0, 19, 230, 100, 7, CYAN);
+  //  tft.setTextColor(YELLOW);
+  //  tft.setCursor(80, 75);
+  //  tft.setTextSize(2);
+  //  tft.print("100");
+  //  tft.setCursor(80, 113);
+  //  tft.setTextSize(2);
+  //  tft.print("100");
+  //GPU
+  showmsgXY(238, 38, 1, &FreeSans12pt7b, "Radeon RX580 Series", RED, 0, 0);
+  showmsgXY(232, 70, 1, &FreeSans12pt7b, "Load :", RED, 0, 0);
+  showmsgXY(445, 74, 1, &FreeSans12pt7b, "%", RED, 0, 0);
+  showmsgXY(232, 105, 1, &FreeSans12pt7b, "Temp:", RED, 0, 0);
+  showmsgXY(445, 113, 1, &FreeSans12pt7b, "*C", RED, 0, 0);
+  tft.drawFastHLine(231, 40, 249 , RED);
+  tft.drawRoundRect(231, 19, 249, 100, 7, RED);
+  //  tft.setTextColor(YELLOW);
+  //  tft.setCursor(320, 75);
+  //  tft.setTextSize(2);
+  //  tft.print("100");
+  //  tft.setCursor(320, 113);
+  //  tft.setTextSize(2);
+  //  tft.print("100");
+  //RAM
+  showmsgXY(90, 138, 1, &FreeSans12pt7b, "RAM", GREEN, 0, 0);
+  showmsgXY(8, 162, 1, &FreeSans12pt7b, "USED:", GREEN, 0, 0);
+  showmsgXY(180, 162, 1, &FreeSans12pt7b, "GB", GREEN, 0, 0);
+  tft.drawFastHLine(0, 140, 230 , GREEN);
+  tft.drawRoundRect(0, 120, 230, 48, 7, GREEN);
+  //INTERNET
+  showmsgXY(60, 188, 1, &FreeSans12pt7b, "INTERNET", MAGENTA, 0, 0);
+  showmsgXY(40, 213, 1, &FreeSans12pt7b, "0.00/0.00", YELLOW, 0, 0);
+  showmsgXY(160, 213, 1, &FreeSans12pt7b, "Mbps", MAGENTA, 0, 0);
+  tft.drawFastHLine(0, 190, 230 , MAGENTA);
+  tft.drawRoundRect(0, 169, 230, 50, 7, MAGENTA);
+  //TIME
+  showmsgXY(90, 239, 1, &FreeSans12pt7b, "TIME", WHITE, 0, 0);
+  showmsgXY(3, 295, 1, &FreeSevenSegNumFont, "12 59 59", WHITE, 0, 0);
+  showmsgXY(45, 316, 1, &FreeSans12pt7b, "12/12/2020", WHITE, 0, 0);
+  tft.drawFastHLine(0, 241, 230 , WHITE);
+  tft.drawRoundRect(0, 220, 230, 100, 7, WHITE);
+  //TEMP
+  showmsgXY(310, 138, 1, &FreeSans12pt7b, "SENSOR", YELLOW, 0, 0);
+  tft.drawFastHLine(231, 140, 249 , YELLOW);
+  tft.drawRoundRect(231, 119, 249, 200, 7, YELLOW);
+}
+void showmsgXY(int x, int y, int sz, const GFXfont *f, const char *msg, int cl, int wd, int hg)
 {
   tft.setFont(f);
   tft.setCursor(x, y);
   tft.setTextSize(sz);
+  tft.setTextColor(cl);
+  if (wd > 0 && hg > 0) {
+    tft.fillRect(x, y - 25, wd, hg, BLACK);
+  }
   tft.print(msg);
 }
